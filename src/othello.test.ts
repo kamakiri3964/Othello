367a9dd4a13aaa1b6c312e8a53be50f2d61ca2b2
kamakiri3964 <westpouch@gmail.com>
2021-08-05 00:07:43 +0900

    盤面表示と石数計算完了v1

--- src/othello.test.ts
+++ src/othello.test.ts
@@ -1,4 +1,9 @@
-import { Board, generate_initial_board } from './othello';
+import {
+  Board,
+  calc_score,
+  generate_initial_board,
+  stringify_board,
+} from './othello';
 
 test('generate_initial_board', () => {
   const board = generate_initial_board();
@@ -13,4 +18,34 @@ test('generate_initial_board', () => {
   expect(board.white[4][3]).toBe(false);
   expect(board.white[3][3]).toBe(true);
   expect(board.black_turn).toBe(true);
+});
+
+test('stringify_board', () => {
+  const board = generate_initial_board();
+  const string_board = stringify_board(board);
+  expect(string_board).toBe(
+    `   a b c d e f g h
+   - - - - - - - -
+1 | | | | | | | | |
+2 | | | | | | | | |
+3 | | | | | | | | |
+4 | | | |o|x| | | |
+5 | | | |x|o| | | |
+6 | | | | | | | | |
+7 | | | | | | | | |
+8 | | | | | | | | |
+   - - - - - - - -
+`
+  );
+});
+
+test('calc_score', () => {
+  const board = generate_initial_board();
+  let score = calc_score(board);
+  expect(score[0]).toBe(2);
+  expect(score[1]).toBe(2);
+  board.black[0][0] = true;
+  score = calc_score(board);
+  expect(score[0]).toBe(3);
+  expect(score[1]).toBe(2);
 });
--- src/othello.ts
+++ src/othello.ts
@@ -18,6 +18,7 @@ export type Board = {
 };
 
 export function generate_initial_board(): Board {
+  //盤面初期化
   const black = new Array(8);
   for (let i = 0; i < 8; i++) {
     black[i] = new Array(8).fill(false);
@@ -41,4 +42,51 @@ export function generate_initial_board(): Board {
   };
 
   return board;
+}
+
+export function stringify_board(board: Board): string {
+  //その時の盤面を表示
+  const L = board.black.length;
+  const G = board.black[0].length;
+  let Hyouji = `   a b c d e f g h
+   - - - - - - - -
+`;
+  board.black.forEach((r, i) => {
+    Hyouji = Hyouji + String(i + 1) + ' |';
+    r.forEach((b, j) => {
+      let c = ' ';
+      if (board.white[i]![j]) {
+        c = 'o';
+      }
+      if (b) {
+        c = 'x';
+      }
+      Hyouji = Hyouji + c + '|';
+    });
+    Hyouji = Hyouji + '\n';
+  });
+  Hyouji = Hyouji + '   - - - - - - - -' + '\n';
+  return Hyouji;
+}
+
+// [黒の石数, 白の石数]を返す
+export function calc_score(board: Board): [number, number] {
+  let b_score = 0;
+  let w_score = 0;
+
+  board.black.forEach((r, i) => {
+    r.forEach((b, j) => {
+      if (b) {
+        b_score++;
+      }
+    });
+  });
+  board.white.forEach((r, i) => {
+    r.forEach((b, j) => {
+      if (b) {
+        w_score++;
+      }
+    });
+  });
+  return [b_score, w_score];
 }
