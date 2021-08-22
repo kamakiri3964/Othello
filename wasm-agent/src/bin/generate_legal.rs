97eab22710b7c6de22d52de7d7550fefcd50119d
Kaku Sawada <kaku.sawada.ak@gmail.com>
2021-08-23 01:47:27 +0900

    generate legal

---
+++ wasm-agent/src/bin/generate_legal.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+use wasm_agent::othello::Board;
+
+fn main() {
+    let mut board_json = String::new();
+    io::stdin().read_line(&mut board_json).expect("Failed to read line");
+    loop {
+        match serde_json::from_str::<Board>(&board_json) {
+            Ok(board) => {
+                println!("{}", board.legal());
+            },
+            Err(_) => break
+        }
+        board_json = String::new();
+        io::stdin().read_line(&mut board_json).expect("Failed to read line");
+    }
+}
