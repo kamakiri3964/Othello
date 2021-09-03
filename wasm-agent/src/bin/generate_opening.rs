c90a398b03a39824f99b3713cd8abe1c98811dcd
Sawada Kaku <kaku.sawada.ak@gmail.com>
2021-09-03 14:53:07 +0900

    generate opening

---
+++ wasm-agent/src/bin/generate_opening.rs
@@ -0,0 +1,29 @@
+use wasm_agent::othello::{Board, lsb};
+
+fn gen_board(board: Board, depth: usize) -> Vec<Board> {
+  if depth == 0 {
+    return vec![board];
+  }
+
+  let mut boards = Vec::new();
+  let mut legal = board.legal();
+  let mut h = lsb(legal);
+  while h != 0 {
+      let mut bs = gen_board(board.put_uncheck(h), depth-1);
+      boards.append(&mut bs);
+      legal ^= h;
+      h = lsb(legal);
+  }
+
+  boards
+}
+
+fn main() {
+  let board = Board::new();
+  let boards = gen_board(board, 6);
+  eprintln!("{}", boards.len());
+  for board in boards {
+    let serialized = serde_json::to_string(&board).unwrap();
+    println!("{}", serialized);
+  }
+}
