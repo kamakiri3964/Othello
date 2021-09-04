4128c6a2794d788256682300f21e19f72e30be31
Kaku Sawada <kaku.sawada.ak@gmail.com>
2021-09-04 21:29:21 +0900

    interface for using from JS

--- wasm-agent/src/lib.rs
+++ wasm-agent/src/lib.rs
@@ -5,19 +5,29 @@ pub mod minmax_agent;
 pub mod othello;
 pub mod reverse;
 
+use alphabeta_agent::AlphaBetaAgent;
+use evaluation::count_legal;
+use othello::Board;
+use reverse::init_reverse;
 use wasm_bindgen::prelude::*;
 
+use crate::agent::Agent;
+
 #[wasm_bindgen]
-pub fn greeting() -> String {
-    "hello".to_string()
+pub fn init() {
+    init_reverse();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn it_works() {
-        assert_eq!(greeting(), "hello".to_string());
+#[wasm_bindgen]
+pub fn next_move(s: &str) -> String {
+    let mut agent = AlphaBetaAgent::new(6, count_legal);
+    if let Ok(board) = serde_json::from_str::<Board>(s) {
+        let m = agent.next(&board);
+        let x = m.leading_zeros() / 8;
+        let y = m.leading_zeros() % 8;
+        format!("[{},{}]", x, y)
+    } else {
+        "".to_string()
     }
 }
 
