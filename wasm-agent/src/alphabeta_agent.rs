57a130e498349d62711a2e232e048ff55d405174
Kaku Sawada <kaku.sawada.ak@gmail.com>
2021-09-06 20:26:25 +0900

    memo alpha beta

--- wasm-agent/src/alphabeta_agent.rs
+++ wasm-agent/src/alphabeta_agent.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{
     agent::Agent,
     othello::{lsb, Board},
@@ -51,7 +53,7 @@ impl AlphaBetaAgent {
         while h != 0 {
             let new_b = board.put_uncheck(h);
             let (_, v) = self.eval_by_search(new_b, depth - 1, -limit_max_v, -max_v);
-            if limit_max_v < -v {
+            if limit_max_v <= -v {
                 return (h, -v);
             }
             if max_v < -v {
@@ -66,6 +68,111 @@ impl AlphaBetaAgent {
 }
 
 impl Agent for AlphaBetaAgent {
+    fn next(&mut self, board: &Board) -> u64 {
+        let (hand, _) = self.eval_by_search(
+            board.clone(),
+            self.depth,
+            i32::min_value() + 1,
+            i32::max_value(),
+        );
+        hand
+    }
+}
+
+#[derive(Clone)]
+pub struct MemoAlphaBetaAgent {
+    depth: usize,
+    memo: HashMap<(usize, u128), (i32, i32)>,
+    eval: fn(&Board) -> i32,
+}
+
+impl MemoAlphaBetaAgent {
+    pub fn new(depth: usize, eval: fn(&Board) -> i32) -> Self {
+        MemoAlphaBetaAgent {
+            depth,
+            memo: HashMap::new(),
+            eval,
+        }
+    }
+
+    fn hash(&self, board: &Board) -> u128 {
+        ((board.player as u128) << 64) ^ (board.opponent as u128)
+    }
+
+    /// Returns (next hand (u64), evaluation value (i32))
+    /// when depth is 0, returns (0, evaluation value (i32))
+    fn eval_by_search(
+        &mut self,
+        board: Board,
+        depth: usize,
+        limit_min_v: i32,
+        limit_max_v: i32,
+    ) -> (u64, i32) {
+        n_search();
+        let hash = self.hash(&board);
+        if let Some(&(a, b)) = self.memo.get(&(depth, hash)) {
+            if a == b {
+                return (0, a);
+            } else if limit_max_v < a {
+                return (0, a);
+            } else if b < limit_min_v {
+                return (0, b);
+            }
+
+            // if limit_min_v < a {
+            //     limit_min_v = a;
+            // }
+        }
+        if depth == 0 {
+            let v = (self.eval)(&board);
+            self.memo.insert((depth, hash), (v, v));
+            return (0, v);
+        }
+        let mut legal = board.legal();
+        if legal == 0 {
+            let new_b = Board {
+                player: board.opponent,
+                opponent: board.player,
+                is_player_black: !board.is_player_black,
+            };
+            let (_, v) = self.eval_by_search(new_b, depth - 1, -limit_max_v, -limit_min_v);
+            if -v < limit_min_v {
+                self.memo.insert((depth, hash), (i32::min_value() + 1, -v));
+            } else if -v <= limit_max_v {
+                self.memo.insert((depth, hash), (-v, -v));
+            } else {
+                self.memo.insert((depth, hash), (-v, i32::max_value()));
+            }
+            return (0, -v);
+        }
+        let mut max_v = limit_min_v;
+        let mut best_hand = 0;
+        let mut h = lsb(legal);
+        while h != 0 {
+            let new_b = board.put_uncheck(h);
+            let (_, v) = self.eval_by_search(new_b, depth - 1, -limit_max_v, -max_v);
+            if limit_max_v <= -v {
+                self.memo.insert((depth, hash), (-v, i32::max_value()));
+                return (h, -v);
+            }
+            if max_v < -v {
+                max_v = -v;
+                best_hand = h;
+            }
+            legal ^= h;
+            h = lsb(legal);
+        }
+        if best_hand == 0 {
+            self.memo
+                .insert((depth, hash), (i32::min_value() + 1, max_v));
+        } else {
+            self.memo.insert((depth, hash), (max_v, max_v));
+        }
+        (best_hand, max_v)
+    }
+}
+
+impl Agent for MemoAlphaBetaAgent {
     fn next(&mut self, board: &Board) -> u64 {
         let (hand, _) = self.eval_by_search(
             board.clone(),
@@ -135,6 +242,42 @@ next: X
         for board in boards.iter().take(100) {
             let actual = agent.next(&board);
             let expected = minmax_agent.next(&board);
+            assert_eq!(actual, expected);
+        }
+    }
+
+    // cargo test alphabeta_agent::tests::compare_to_memo --release -- --ignored
+    #[test]
+    #[ignore]
+    fn compare_to_memo() {
+        init_reverse();
+        let depth = 6;
+        let mut agent = AlphaBetaAgent::new(depth, count_stone);
+        let mut memoagent = MemoAlphaBetaAgent::new(depth, count_stone);
+        let mut boards = Vec::new();
+        if let Ok(file) = File::open("./data/random_boards.jsonl") {
+            for result in BufReader::new(file).lines() {
+                if let Ok(board_json) = result {
+                    if let Ok(board) = serde_json::from_str::<Board>(&board_json) {
+                        boards.push(board);
+                    }
+                }
+            }
+        }
+        for (idx, board) in boards.iter().take(100).enumerate() {
+            let actual = agent.next(&board);
+            let expected = memoagent.next(&board);
+            if expected != actual {
+                println!("{}", board);
+                let b = Board {
+                    player: actual,
+                    opponent: expected,
+                    is_player_black: true,
+                };
+                println!("{}", b);
+                println!("idx: {}", idx);
+                assert_eq!(actual, expected);
+            }
             assert_eq!(actual, expected);
         }
     }
