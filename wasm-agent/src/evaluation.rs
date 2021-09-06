6f011c371bd69bd898c7634fbae1bba77a916859
Kaku Sawada <kaku.sawada.ak@gmail.com>
2021-09-06 20:27:32 +0900

    add evaluation

--- wasm-agent/src/evaluation.rs
+++ wasm-agent/src/evaluation.rs
@@ -7,3 +7,20 @@ pub fn count_stone(board: &Board) -> i32 {
 pub fn count_legal(board: &Board) -> i32 {
     board.legal().count_ones() as i32
 }
+
+pub fn sub_legal(board: &Board) -> i32 {
+    let t = Board {
+        player: board.opponent,
+        opponent: board.player,
+        is_player_black: true,
+    };
+    board.legal().count_ones() as i32 - t.legal().count_ones() as i32
+}
+
+pub fn sub_legal_and_last(board: &Board) -> i32 {
+    if (board.player ^ board.opponent).count_zeros() == 0 {
+        board.player.count_ones() as i32
+    } else {
+        sub_legal(board)
+    }
+}
